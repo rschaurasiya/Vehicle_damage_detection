182cfd23af6fd5966659efe160a27bf05558d37a
Radheshyam Chaurasiya <chaurasiyaradheshyamkumar@gmail.com>
2026-09-10 06:52:46 +0530

    Update class names for clarity in model_helper.py

--- streamlit_app/model_helper.py
+++ streamlit_app/model_helper.py
@@ -6,7 +6,7 @@ from pathlib import Path
 
 
 train_model = None
-class_names = ['F_Breakage', 'F_Crushed', 'F_Normal', 'R_Breakage', 'R_Crushed', 'R_Normal']
+class_names = ['Front Breakage', 'Front Crushed', 'Front Normal', 'Rear Breakage', 'Rear Crushed', 'Rear Normal']
 
 
 class CarClassifierwithResNet50(nn.Module):
