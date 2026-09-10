1794b81c574e4663b029a7e14bb788dd9659c88b
Radheshyam Chaurasiya <chaurasiyaradheshyamkumar@gmail.com>
2026-09-10 06:17:31 +0530

    Update app.py

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -6,9 +6,12 @@ st.title("Car Damage Detection")
 uploaded_file = st.file_uploader("Upload the file", type = ["jpg", "png"])
 
 if uploaded_file:
-    image_path = "temp_file.jpg"
-    with open (image_path, "wb") as f:
+    image_path = f"temp_file_{uploaded_file.name}"
+
+    with open(image_path, "wb") as f:
         f.write(uploaded_file.getbuffer())
-        st.image(uploaded_file, caption = "Uploaded File", use_container_width = True)
-        prediction = predict (image_path)
-        st.info(f"Predicted class is: {prediction}")
+
+    st.image(uploaded_file, caption="Uploaded File", use_container_width=True)
+
+    prediction = predict(image_path)
+    st.info(f"Predicted class is: {prediction}")
